c5e0c449315863ceb95b31c6cdde20c34a583342
Andrin <andrinjosua.schaerli@gmail.com>
2025-07-05 21:08:40 +0200

    Erste Programmierung

---
+++ sites/fact.php
@@ -0,0 +1,4 @@
+<?php
+$id = $_GET['id'] ?? '';
+echo "<h1>Fact: $id</h1>";
+?>
---
+++ sites/main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faktenchecker - Startseite</title>
+</head>
+<body>
+    <h1>Hallo Welt</h1>
+    <p>Herzlich wilkommen beim Faktencheck</p>
+</body>
+</html>
